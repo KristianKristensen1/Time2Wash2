7e508891fe2bbcd9bae57756a3f6e94193e5d87b
CasperDam <38529338+CasperDam@users.noreply.github.com>
2018-04-26 14:27:34 +0200

    Resources - Menu layout

--- app/src/main/java/com/example/krist/time2washproject/ProfileActivity.java
+++ app/src/main/java/com/example/krist/time2washproject/ProfileActivity.java
@@ -2,12 +2,19 @@ package com.example.krist.time2washproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.Menu;
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    /*https://www.youtube.com/watch?v=dvWrniwBJUw*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
