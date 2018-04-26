1d9d73592c943dab55889fcc8cb51314f83e0d98
CasperDam <38529338+CasperDam@users.noreply.github.com>
2018-04-26 15:20:22 +0200

    Booking Activity Layout Initial

--- app/src/main/java/com/example/krist/time2washproject/BookingActivity.java
+++ app/src/main/java/com/example/krist/time2washproject/BookingActivity.java
@@ -2,12 +2,36 @@ package com.example.krist.time2washproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class BookingActivity extends AppCompatActivity {
+    String[] machineList = {"Vask1", "Vask2", "Tør1", "Tør2"}, dateList = {"Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"};
+    ArrayAdapter<String> machineArrayAdapter, dateArrayAdapter;
+    MaterialBetterSpinner machineBetterSpinner, dateBetterSpinner;
+    com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner bookingActivity_chooseMachine_dropDown, bookingActivity_chooseDate_dropDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+        findViews();
+        setDropDowns();
+    }
+
+    /*https://www.youtube.com/watch?v=x6HtXktAoew*/
+    private void setDropDowns() {
+        machineArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, machineList);
+        dateArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dateList);
+        machineBetterSpinner = bookingActivity_chooseMachine_dropDown;
+        dateBetterSpinner = bookingActivity_chooseDate_dropDown;
+        machineBetterSpinner.setAdapter(machineArrayAdapter);
+        dateBetterSpinner.setAdapter(dateArrayAdapter);
+    }
+
+    private void findViews() {
+        bookingActivity_chooseMachine_dropDown = findViewById(R.id.bookingActivity_chooseMachine_dropDown);
+        bookingActivity_chooseDate_dropDown = findViewById(R.id.bookingActivity_chooseDate_dropDown);
     }
 }
